6b8b973f3893201040c3177dc3d596ef78ab664a
Timothy Overend <timothyoverend@gmail.com>
2018-10-28 20:29:58 -0400

    finished the assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+var dadFirstName="Kevin";
+var dadLastName="Overend";
+var dadAge=51;
+var dadFavoriteTVShow="None";
+var dadFavoriteSong="Doesn't have one";
+
+var momFirstName="Rebecca";
+var momLastName="Overend";
+var momAge=52;
+var momFavoriteTVShow="None";
+var momFavoriteSong="Even If";
+
+var myFirstName="Timothy";
+var myLastName="Overend";
+var myAge=16;
+var myFavoriteTVShow="None";
+var myFavoriteSong="Shoulders";
+
+var family=[myFavoriteSong,myFavoriteTVShow,myAge,myLastName,myFirstName,omFavoriteSong,momFavoriteTVShow,momAge,momLastName,momFirstName,dadFavoriteSong,dadFavoriteTVShow,dadAge,dadLastName,dadFirstName];
+console.log(family);
+for(var i=0;i<1;i++){
+    console.log(dadFirstName+" "+dadFavoriteSong);
+    console.log(momFirstName+" "+momFavoriteSong);
+    console.log(myFirstName+" "+myFavoriteSong);
+
+}
+function favSongUpdate(){
+    console.log("updates the favorite TVShow of the family member once a member of the family is passed in as a function parameter?");
+}
+
+
+    console.log(myFavoriteTVShow+" "+momFavoriteTVShow+" "+dadFavoriteTVShow);
